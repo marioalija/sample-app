80acf8d40ec0e88d14a3fbfe0fde672e55abd64b
marioalija <marioalija25@gmail.com>
2023-10-20 11:49:20 -0500

    -mroutes.rb

--- app/helpers/example_pages_helper.rb
+++ app/helpers/example_pages_helper.rb
@@ -1,2 +1,5 @@
 module ExamplePagesHelper
+  def hello_method
+    render json: { mesage: "hello world" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/hello_path", controller: "example_pages", action: "hello_method"
 end
